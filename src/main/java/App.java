bec65e8956f639860739ec1ff903ce6b47fc7543
Noel Bor <phaxier@gmail.com>
2019-06-04 15:58:57 +0300

    upadting dependancies

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+
+
+public class App {
+
+
+        public static void main(String[] args) {
+            staticFileLocation("/public");
+            String layout = "templates/layout.vtl";
+
+
+    }
+}
+
